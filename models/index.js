1cb74eadacb160cd7c2c276372d60cb60e6629b4
Bakairou <bakairou@gmail.com>
2023-06-01 19:31:05 -0400

    Index and Models changed

--- models/index.js
+++ models/index.js
@@ -2,5 +2,13 @@ const Account = require('./User');
 const Item = require('./Item');
 const Review= require('./Review');
 
+Account.hasMany(Order, { foreignKey: 'accountId' });
+Order.belongsTo(Account, { foreignKey: 'accountId' });
+
+Item.hasMany(Order, { foreignKey: 'itemId' });
+Order.belongsTo(Item, { foreignKey: 'itemId' });
+
+
 
 module.exports = { User, Item, Review };
+
